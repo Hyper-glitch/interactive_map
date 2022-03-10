2668c179ce753d71efcf2604c952c40fbdb9cf2b
Roman <graff049@gmail.com>
2022-03-10 23:17:50 +0300

    1. register place app 2. create place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(max_length=200)
+    description_short = models.CharField(max_length=500)
+    description_long = models.TextField()
