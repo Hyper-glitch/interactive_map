d5e65598f2f13ba21dec3bcc779c1ee4e9833e18
Roman <graff049@gmail.com>
2022-06-05 22:14:07 +0300

    create model for places

--- places/models.py
+++ places/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from jsonfield import JSONField
 
 
 class Place(models.Model):
     title = models.CharField(max_length=200)
     description_short = models.CharField(max_length=500)
     description_long = models.TextField()
+    coordinates = JSONField()
+
+    def __str__(self):
+        return self.title
