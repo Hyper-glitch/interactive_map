97ebb3d87da1b1e7de7749783491bbcb4308453a
Roman <graff049@gmail.com>
2022-06-06 19:49:04 +0300

    add model for images

--- places/models.py
+++ places/models.py
@@ -10,3 +10,11 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='places')
+
+    def __str__(self):
+        return self.name
