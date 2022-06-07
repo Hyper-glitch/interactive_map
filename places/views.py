0432dcbad10d074442af2de7bcb074ec9f187398
Roman <graff049@gmail.com>
2022-06-07 21:41:03 +0300

    prepare places info in view

--- places/views.py
+++ places/views.py
@@ -1,45 +1,30 @@
 from django.shortcuts import render
-
+from django.core import serializers
 from places.models import Place
 
 
 def index(request):
-    {
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.62, 55.793676]
-          },
-          "properties": {
-            "title": "«Легенды Москвы",
-            "placeId": "moscow_legends",
-            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
-          }
-        },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64, 55.753676]
-          },
-          "properties": {
-            "title": "Крыши24.рф",
-            "placeId": "roofs24",
-            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
-          }
-        }
-      ]
+    data = {
+        "type": "FeatureCollection",
+        "features": get_json_places(request),
     }
+    return render(request, 'index.html', context=data)
 
-    return render(request, 'index.html')
 
-
-def get_json_place(request):
+def get_json_places(request):
     places = Place.objects.all()
+    features = []
+    place_id = 0
+
     for place in places:
+        images = place.images.all()
+        detailed_places_info = {
+            "title": place.title,
+            "imgs": [image.image.url for image in images],
+            "description_short": place.description_short,
+            "description_long": place.description_long,
+            "coordinates": place.coordinates,
+        }
 
         place_info = {
             "type": "Feature",
@@ -48,8 +33,12 @@ def get_json_place(request):
                 "coordinates": place.get_coordinates()
             },
             "properties": {
-                "title": "«Легенды Москвы",
-                "placeId": "moscow_legends",
-                "detailsUrl": ""
+                "title": place.title,
+                "placeId": place_id,
+                "detailsUrl": detailed_places_info
             }
         }
+        features.append(place_info)
+        place_id += 1
+
+    return features
