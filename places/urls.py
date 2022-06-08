1cae0044d5079c18933fd3877ae201031d5b8405
Roman <graff049@gmail.com>
2022-06-08 21:54:59 +0300

    add new endpoint

--- places/urls.py
+++ places/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('get_json_place/<place_pk>', views.get_json_place, name='get_json_place'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
