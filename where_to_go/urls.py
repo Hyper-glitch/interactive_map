a0d07912679a0d6a7603168f180424b29339ea15
Roman Smirnov <r.smirnov@okenit.ru>
2022-06-10 11:32:11 +0300

    add static to urls

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('places.urls')),
                   path('tinymce/', include('tinymce.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+              static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
