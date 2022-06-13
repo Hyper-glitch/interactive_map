ea5494c6a09276a7fc580e4be4a0431f434cd09f
Roman <graff049@gmail.com>
2022-06-13 13:34:24 +0300

    make load place for file with urls

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -8,28 +8,35 @@ from places.models import Place, Image
 
 
 class Command(BaseCommand):
-    help = 'Add a new place to DataBase'
+    help = 'Add a new places to DataBase'
 
     def add_arguments(self, parser):
-        parser.add_argument('json_url', nargs='+', type=str)
+        parser.add_argument('places_urls', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        url = options['json_url'][0]
-        response = requests.get(url=url)
-        response.raise_for_status()
-        place = response.json()
-        place_obj = Place.objects.get_or_create(
-            title=place['title'], description_short=place['description_short'],
-            description_long=['description_long'], coordinates=place['coordinates'],
-        )
-        for img in place['imgs']:
-            position = 1
-            save_place_img(img_url=img, place=place_obj[0], position=position)
-            position += 1
+        path = options['places_urls'][0]
+
+        with open(path) as places_urls:
+            urls = places_urls.readlines()
+
+            for url in urls:
+                response = requests.get(url=url.rstrip('\n'))
+                response.raise_for_status()
+                place = response.json()
+                place_obj = Place.objects.get_or_create(
+                    title=place['title'], description_short=place['description_short'],
+                    description_long=place['description_long'], coordinates=place['coordinates'],
+                )
+                for img in place['imgs']:
+                    position = 1
+                    save_place_img(img_url=img, place=place_obj[0], position=position)
+                    position += 1
 
 
 def save_place_img(img_url, place, position):
     filename = str(uuid.uuid4())
     response = requests.get(img_url)
     response.raise_for_status()
-    Image.objects.create(place=place, name=filename, image=ContentFile(response.content), position=position)
+    content = ContentFile(response.content)
+    img = Image.objects.create(place=place, name=filename, position=position)
+    img.image.save(filename, content, save=True)
