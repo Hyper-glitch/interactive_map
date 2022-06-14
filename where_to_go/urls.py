bd6037e5ef435607c9a6ce279e02dc629615221e
Roman <graff049@gmail.com>
2022-06-14 23:43:21 +0300

    use flake8 and fix its issues  E501 line too long (83 > 79 characters)

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -19,8 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', include('places.urls')),
-                  path('tinymce/', include('tinymce.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
-              static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include('places.urls')),
+    path('tinymce/', include('tinymce.urls')),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
