0a3572875ff79999ef10e99caa9ede8f459fcab5
Roman Smirnov <r.smirnov@okenit.ru>
2022-06-15 12:21:19 +0300

    add / to the end of urls

--- places/urls.py
+++ places/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('place/<place_pk>', views.get_json_place, name='get_json_place'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('', views.index, name='index'),
+                  path('place/<place_pk>/', views.get_json_place, name='get_json_place'),
+              ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
