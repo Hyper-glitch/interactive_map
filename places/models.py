8c300392d3ff9fd8376022025e39c67d5c150f94
Roman Smirnov <r.smirnov@okenit.ru>
2022-06-15 10:15:24 +0300

    fix Django Best Practices Models

--- places/models.py
+++ places/models.py
@@ -1,18 +1,18 @@
 from django.db import models
-from tinymce.models import HTMLField
 from jsonfield import JSONField
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    class Meta:
-        ordering = ['order']
-        unique_together = ['title', 'id']
-
     order = models.PositiveIntegerField(default=1)
     title = models.CharField(max_length=200)
     description_short = models.CharField(max_length=500)
     description_long = HTMLField()
     coordinates = JSONField()
+
+    class Meta:
+        ordering = ['order']
+        unique_together = ['title', 'id']
 
     def __str__(self):
         return self.title
@@ -23,13 +23,13 @@ class Place(models.Model):
 
 
 class Image(models.Model):
-    class Meta:
-        ordering = ["position"]
-
     name = models.CharField(max_length=200)
     image = models.ImageField(upload_to='places_pics', null=True)
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
     position = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        ordering = ["position"]
 
     def __str__(self):
         return f'{self.position} {self.name}'
