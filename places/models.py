d4c91aef53d575c9fac16e10fcfc0217f832dcc5
Roman Smirnov <r.smirnov@okenit.ru>
2022-06-15 10:51:59 +0300

    make fields more suitable for content managers

--- places/models.py
+++ places/models.py
@@ -6,8 +6,8 @@ from tinymce.models import HTMLField
 class Place(models.Model):
     order = models.PositiveIntegerField(default=1)
     title = models.CharField(max_length=200)
-    description_short = models.TextField()
-    description_long = HTMLField()
+    description_short = models.TextField(blank=True, null=True)
+    description_long = HTMLField(blank=True, null=True)
     coordinates = JSONField()
 
     class Meta:
