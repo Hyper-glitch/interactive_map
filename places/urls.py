86048529a8433c440ceb56ed66eeb4e3b9312221
Roman Smirnov <r.smirnov@okenit.ru>
2022-06-15 12:21:49 +0300

    refactor tabulation PEP8

--- places/urls.py
+++ places/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-                  path('', views.index, name='index'),
-                  path('place/<place_pk>/', views.get_json_place, name='get_json_place'),
-              ]
+    path('', views.index, name='index'),
+    path('place/<place_pk>/', views.get_json_place, name='get_json_place'),
+]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
