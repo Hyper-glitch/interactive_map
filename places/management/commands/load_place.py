f64cee20980664ab47977c9ebfc05c4b7779be31
Roman <graff049@gmail.com>
2022-06-16 23:42:48 +0300

    1. remove multiple paths for args 2. close context manager exactly after using

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -12,32 +12,32 @@ class Command(BaseCommand):
     help = 'Add a new places to DataBase'
 
     def add_arguments(self, parser):
-        parser.add_argument('places_urls', nargs='+', type=str)
+        parser.add_argument('places_urls', type=str)
 
     def handle(self, *args, **options):
         """ Read file with urls and create places with json info."""
-        path = options['places_urls'][0]
+        path = options['places_urls']
 
         with open(path) as places_urls:
             urls = places_urls.readlines()
 
-            for url in urls:
-                response = requests.get(url=url.rstrip('\n'))
-                response.raise_for_status()
-                place = response.json()
-                place_obj, created = Place.objects.get_or_create(
-                    title=place['title'],
-                    defaults={
-                        'description_short': place['description_short'],
-                        'description_long': place['description_long'],
-                        'latitude': place['coordinates']['lat'],
-                        'longitude': place['coordinates']['lng'],
-                    }
-                )
-                for img in place['imgs']:
-                    position = 1
-                    save_place_img(img_url=img, place=place_obj, position=position)
-                    position += 1
+        for url in urls:
+            response = requests.get(url=url.rstrip('\n'))
+            response.raise_for_status()
+            place = response.json()
+            place_obj, created = Place.objects.get_or_create(
+                title=place['title'],
+                defaults={
+                    'description_short': place['description_short'],
+                    'description_long': place['description_long'],
+                    'latitude': place['coordinates']['lat'],
+                    'longitude': place['coordinates']['lng'],
+                }
+            )
+            for img in place['imgs']:
+                position = 1
+                save_place_img(img_url=img, place=place_obj, position=position)
+                position += 1
 
 
 def save_place_img(img_url, place, position):
