782c8ae4415a4afe3eb3a3caa186307bc26fd543
Roman <graff049@gmail.com>
2022-06-16 22:09:51 +0300

    del extra var for static

--- places/urls.py
+++ places/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -8,4 +6,3 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('place/<place_pk>/', views.get_json_place, name='get_json_place'),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
